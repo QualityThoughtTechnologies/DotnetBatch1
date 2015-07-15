0d2f1b09fb80399eced5462080c8f06625ce7f22
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-07-15 08:50:23 +0530

    Added Code

---
+++ Foundation/Foundation_15July_Interface/HardwareLibrary/Processor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareLibrary
+{
+    public class Processor:IHardwareDevice
+    {
+
+        public Processor()
+        {
+            Socket = SocketType.ThirtyTwoPin;
+        }
+
+        public string Describe()
+        {
+            return "Processor";
+
+        }
+        public SocketType Socket { set; get; }
+    }
+}
---
+++ Foundation/Foundation_15July_Interface/InterfaceConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using HardwareLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RAM ram = new RAM();
+            Processor processor = new Processor();
+            MotherBoard motherBoard = new MotherBoard(2);
+            motherBoard.AddHardware(ram);
+            motherBoard.AddHardware(processor);
+            Console.WriteLine(motherBoard.Display());
+        }
+    }
+}

2c901512097849ae1a022c2bfedcf3353d70ec29
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-07-21 08:25:58 +0530

    LIst introduction

---
+++ Foundation/Foundation_20July/Vehicle/ParkingStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class ParkingStation
+    {
+        IList<IVehicle> vehicles = new List<IVehicle>(3);
+
+        public void AddVehicle(IVehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        public void DisplayVehicleBills()
+        {
+            foreach(IVehicle vehicle in vehicles)
+            {
+                Console.WriteLine(" The Parking amount is " + vehicle.GetParkingAmount(14, 50));
+            }
+        }
+    }
+}
---
+++ Foundation/Foundation_20July/Vehicle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingStation parkingStation = new ParkingStation();
+            IVehicle bike=new Bike();
+            bike.SetEntryTime(6,0);
+            parkingStation.AddVehicle(bike);
+
+            IVehicle cycle = new Bicycle();
+            cycle.SetEntryTime(7, 0);
+            parkingStation.AddVehicle(cycle);
+
+            parkingStation.DisplayVehicleBills();
+        }
+    }
+}

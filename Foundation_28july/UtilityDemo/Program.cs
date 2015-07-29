73bb078fceb113b308328f1ddb63ed693721b3c3
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-07-29 08:24:23 +0530

    Added Parse & Pass by reference

---
+++ Foundation_28july/UtilityDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityLibrary;
+
+namespace UtilityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rateOfINtrest1;
+            double si1=IntrestUtility.GetSimpleIntrest(1000.0, 1.0, out rateOfINtrest1);
+            Console.WriteLine("Simple Intrest is {0} and rate of intrest is {1}",si1,rateOfINtrest1);
+
+            double rateOfIntrest2 = 5;
+            double si2 = IntrestUtility.GetSimpleIntrestWithRateOfINtrestEntered(1000.0, 1.0, ref rateOfIntrest2);
+            Console.WriteLine("Simple Intrest is {0} and rate of intrest is {1}", si2, rateOfIntrest2);
+        }
+    }
+}
---
+++ Foundation_28july/UtilityLibrary/IntrestUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLibrary
+{
+    public static class IntrestUtility
+    {
+        public static double GetSimpleIntrest(double principal,double timeInYears,out double rateOfIntrest)
+        {
+            //assume rate of intrest
+            rateOfIntrest = 6.0;
+            return (principal * timeInYears * rateOfIntrest) / 100;
+        }
+
+
+
+        public static double GetSimpleIntrestWithRateOfINtrestEntered(double principal, double timeInYears, ref double rateOfIntrest)
+        {
+            //assume rate of intrest
+            rateOfIntrest--;
+            return (principal * timeInYears * rateOfIntrest) / 100;
+        }
+
+    }
+}

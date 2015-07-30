e8e89a7623f1f6c3c94dabeff9844f38c8f7b014
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-07-30 08:34:24 +0530

    initial version of Exception Demo

---
+++ Foundation/Foundation_30July/QualityThought.Samples.DotNetBatch1.ExceptionDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityThought.Samples.DotNetBatch1.ExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int value = int.Parse(input);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Invalid input");
+            }
+            
+        }
+    }
+}

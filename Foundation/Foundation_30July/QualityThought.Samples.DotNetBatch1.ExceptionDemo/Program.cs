37bfd85870d0091b111a462c5305c18404158f02
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-07-31 08:48:16 +0530

    exception handling

--- Foundation/Foundation_30July/QualityThought.Samples.DotNetBatch1.ExceptionDemo/Program.cs
+++ Foundation/Foundation_30July/QualityThought.Samples.DotNetBatch1.ExceptionDemo/Program.cs
@@ -12,15 +12,47 @@ namespace QualityThought.Samples.DotNetBatch1.ExceptionDemo
         {
             Console.WriteLine("Enter any number");
             string input = Console.ReadLine();
+            if(input=="NULL")
+            {
+                input = null;
+            }
 
             try
             {
                 int value = int.Parse(input);
             }
-            catch(Exception ex)
+            
+            catch(ArgumentNullException)
+            {
+                Console.WriteLine("Invalid input: PLease enter any value");
+            }
+            catch(FormatException e)
             {
                 Console.WriteLine("Invalid input");
+                Console.WriteLine(e.StackTrace);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                Student student = new Student();
+                student.Name = "Test";
+                student.GetDetails();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Please Visit us again");
+            }
+           
             
         }
     }
---
+++ Foundation/Foundation_30July/QualityThought.Samples.DotNetBatch1.ExceptionDemo/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityThought.Samples.DotNetBatch1.ExceptionDemo
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This shows details
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// An Invalid Operation Exception is thrown when name is empty
+        /// </exception>
+        public void GetDetails()
+        {
+            if(string.IsNullOrWhiteSpace(Name))
+            {
+                throw new InvalidOperationException("Please Enter a valid Name");
+            }
+        }
+    }
+}

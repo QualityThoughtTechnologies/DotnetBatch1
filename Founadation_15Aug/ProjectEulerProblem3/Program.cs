bcbca79ed50a546ee44433f1d301a56b9200df1c
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-08-15 10:21:29 +0530

    Project Euler problem 3

---
+++ Founadation_15Aug/ProjectEulerProblem3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input");
+            int number = int.Parse(Console.ReadLine());
+            for(int index=2;index<number;index++)
+            {
+                if(number%index==0)
+                {
+                    Console.Write(index + " ");
+                }
+            }
+        }
+    }
+}
